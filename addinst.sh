4aa92e065cc12065a0bdd49c29253b5088544812
keikun <ko.kidong@gmail.com>
2016-12-12 09:58:12 +0000

    initial

---
+++ addinst.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+
+function addInst() { 
+
+    shutdownport=8005
+    httpport=8080
+    httpsport=8443
+    redport=${httpsport}
+    ajpport=8109
+    #INSTROOT=`pwd` 
+    #INSTDIR=${INSTROOT}/instance/${INSTNAME}
+    #CONFDIR="conf/"  
+    #CONFIG="sia.conf"
+    #CONF=${CONFDIR}${CONFIG}    
+
+    if [ -d ${INSTDIR} ]; then  
+        echo "Already exist Instance name (${INSTNAME})"
+        exit
+    fi 
+ 
+    echo "Instnace name : " ${INSTNAME} 
+    echo "Shutdown port (default : 8005) : " 
+    read a
+    if [[ $a ]]; then
+       shutdownport=${a} 
+    fi 
+    echo "http port (default : 8080) : "
+    read b 
+    if [[ $b ]]; then
+       httpport=${b} 
+    fi 
+    echo "https port (default : 8443) : "
+    read c
+    if [[ $c ]]; then
+       httpsport=${c} 
+    fi 
+    echo "redirect port (default : 8443) : "
+    read d 
+    if [[ $c ]]; then
+      redport=${d} 
+    fi 
+    echo "ajp port (default : 8109) : "
+    read e  
+    if [[ $e ]]; then
+      ajpport=${e} 
+    fi 
+
+    echo -e "add instance ${INSTNAME}"
+    mkdir -p ${INSTDIR}
+    mkdir -p ${INSTDIR}/${CONFDIR}
+    mkdir -p ${INSTDIR}/lib
+    mkdir -p ${INSTDIR}/logs
+    mkdir -p ${INSTDIR}/temp
+    mkdir -p ${INSTDIR}/work
+    mkdir -p ${INSTDIR}/webapps
+
+    
+
+#    cat > .tempconfig  << EOF
+##!/bin/bash
+#sia_instname=${INSTNAME}
+#sia_shutdown=${shutdownport}
+#sia_http=${httpport}
+#sia_https=${httpsport}
+#sia_redirect=${redport}
+#sia_ajp=${ajpport} 
+#sia_jvmroute=${INSTNAME}
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.shutdown=\$sia_instname
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.http=\$sia_shutdown
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.https=\$sia_http
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.redirect=\$sia_https
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.ajp=\$sia_ajp
+#export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.jvmroute=\$sia_instname
+#EOF
+    echo -e "#!/bin/bash"  >>                                                       ${INSTDIR}/${CONF}
+    echo -e "sia_instname=${INSTNAME} " >>                                          ${INSTDIR}/${CONF}
+    echo -e "sia_shutdown=${shutdownport}" >>                                       ${INSTDIR}/${CONF}
+    echo -e "sia_http=${httpport}" >>                                               ${INSTDIR}/${CONF}
+    echo -e "sia_https=${httpsport}" >>                                             ${INSTDIR}/${CONF}
+    echo -e "sia_redirect=${redport}" >>                                            ${INSTDIR}/${CONF}
+    echo -e "sia_ajp=${ajpport} " >>                                                ${INSTDIR}/${CONF}
+    echo -e "sia_jvmroute=${INSTNAME}" >>                                           ${INSTDIR}/${CONF} 
+    
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.shutdown=\$sia_shutdown\"" >> ${INSTDIR}/${CONF}
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.http=\$sia_http\"" >>     ${INSTDIR}/${CONF}
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.https=\$sia_https\"" >>        ${INSTDIR}/${CONF}
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.redirect=\$sia_redirect\"">>     ${INSTDIR}/${CONF}
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.ajp=\$sia_ajp\"" >>           ${INSTDIR}/${CONF}
+    echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.jvmroute=\$sia_instname\"" >> ${INSTDIR}/${CONF} 
+
+    cp ${INSTROOT}/share/server.xml.dist ${INSTDIR}/${CONFDIR}/server.xml
+
+} 
+function removeInst() {  
+    `rm -rf ${INSTDIR}`
+}
+
+
+INSTNAME=$1 
+ARGV=$2   
+#currentDir=`pwd`
+INSTROOT=`pwd` 
+INSTDIR=${INSTROOT}/instance/${INSTNAME}
+CONFDIR="conf/"  
+CONFIG="sia.conf"
+CONF=${CONFDIR}${CONFIG}     
+
+if [ "$#" -ne 2 ]; then  
+    echo "Usage : instadd.sh [Instance] {add | remove}"
+fi
+case $ARGV in 
+
+add)
+    addInst
+    ;;
+remove)
+   removeInst  
+    ;;
+esac
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash  
+
+
+currentDir=`pwd`;
+echo ${JAVA_HOME} 
+echo ${CLASSPATH} 
+temp_java_home=${JAVA_HOME}
+# check JAVA_OPT 
+
+if [[ ! ${JAVA_HOME} ]]; then 
+    echo "enter JAVA_HOME for tomcat"
+    read a
+    if [[ $a ]]; then
+        temp_java_home=${a}    
+    fi
+fi
+  
+`cp ./launcher.sh.dist ./launcher.sh` 
+sed -i -e "s:CHANGE_JAVA_HOME:${temp_java_home}:g" launcher.sh
+#echo ${temp_java_home}  
+#echo -e 'export JAVA_HOME='${temp_java_home} >> .setenv 
+
+# else input JAVA_OPT 
+
+
+#check PATH include java
+
