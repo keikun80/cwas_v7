d9763d5ca1f2c8ffbb6c9254bf00ea7fb518b977
keikun <ko.kidong@gmail.com>
2016-12-17 08:56:46 +0000

    update scripts

--- addinst.sh
+++ addinst.sh
@@ -1,52 +1,81 @@
 #!/bin/bash
 
+function _front()
+{ 
+    TITLE="CHLUX INSTANCE CREATOR"
+    clear
+    eval printf %.0s\# '{1..'${COLUMNS:-$(tput cols)}'}'; echo    
+    echo -e "" 
+    printf "%*s\n" $(((${#TITLE}+$(tput cols))/2)) "$TITLE"
+    echo -e "\t Version : 1.0" 
+    echo -e "\t Author  : Chlux Co,Ltd."
+    echo -e "\t Release : 25. Dec. 2016" 
+    echo -e "\t Package : TOMCAT"
+    echo -e ""
+    #echo -e "\t INSTANCE NAME : "${INSTNAME}
+    #echo -e "\t ACTION        : "$2 
+    eval printf %.0s\# '{1..'${COLUMNS:-$(tput cols)}'}'; echo  
+} 
 
-function addInst() { 
-
+function _footer()
+{ 
+echo "FOOTER"
+}
+function _setDefault()
+{
     shutdownport=8005
     httpport=8080
     httpsport=8443
     redport=${httpsport}
     ajpport=8109
+}
+function addInst() { 
+
     #INSTROOT=`pwd` 
     #INSTDIR=${INSTROOT}/instance/${INSTNAME}
     #CONFDIR="conf/"  
     #CONFIG="sia.conf"
     #CONF=${CONFDIR}${CONFIG}    
-
+    
+    if [ ! -f .javaopts ]; then  
+        echo -e "\e[31mCiritical] No file for setup JAVA_OTPS . Please run installer first\e[0m" 
+        exit;
+    fi 
     if [ -d ${INSTDIR} ]; then  
-        echo "Already exist Instance name (${INSTNAME})"
+        echo -e "\e[33mAlready exist Instance name (${INSTNAME})\e[0m"
+        echo -e "\e[93mExit script\e[0m" 
         exit
     fi 
  
-    echo -e "Instnace name : " ${INSTNAME} 
-    echo -n "Shutdown port (default : 8005) : " 
+    echo -e "\e[32mInstnace name : \e[33m" ${INSTNAME}"\e[0m"
+    echo -e -n "\e[32mShutdown port (default : 8005) : \e[0m" 
     read a
     if [[ $a ]]; then
        shutdownport=${a} 
     fi 
-    echo -n "http port (default : 8080) : "
+    echo -e -n "\e[32mhttp port (default : 8080) : \e[0m"
     read b 
     if [[ $b ]]; then
        httpport=${b} 
     fi 
-    echo -n "https port (default : 8443) : "
+    echo -e -n "\e[32mhttps port (default : 8443) : \e[0m"
     read c
     if [[ $c ]]; then
        httpsport=${c} 
     fi 
-    echo -n "redirect port (default : 8443) : "
+    echo -e -n "\e[32mredirect port (default : 8443) : \e[0m"
     read d 
     if [[ $c ]]; then
       redport=${d} 
     fi 
-    echo -n "ajp port (default : 8109) : "
+    echo -e -n "\e[32majp port (default : 8109) : \e[0m"
     read e  
     if [[ $e ]]; then
       ajpport=${e} 
     fi 
 
-    echo -e "add instance ${INSTNAME}"
+    #echo -e "\e[32madd instance ${INSTNAME} \e[0m"
+    echo -e "\e[32mcreate instance directory for  \e[33m${INSTNAME} \e[0m"
     mkdir -p ${INSTDIR}
     mkdir -p ${INSTDIR}/${CONFDIR}
     mkdir -p ${INSTDIR}/lib
@@ -72,7 +101,9 @@ function addInst() {
 #export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.redirect=\$sia_https
 #export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.ajp=\$sia_ajp
 #export JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.jvmroute=\$sia_instname
-#EOF
+#EOF  
+    echo -e "\e[32mcreate instance configuration for  \e[33m${INSTNAME} \e[0m"
+
     echo -e "#!/bin/bash"  >>                                                       ${INSTDIR}/${CONF}
     echo -e "sia_instname=\"${INSTNAME}\"" >>                                          ${INSTDIR}/${CONF}
     echo -e "sia_shutdown=${shutdownport}" >>                                       ${INSTDIR}/${CONF}
@@ -88,7 +119,8 @@ function addInst() {
     echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.redirect=\$sia_redirect\"">>     ${INSTDIR}/${CONF}
     echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.ajp=\$sia_ajp\"" >>           ${INSTDIR}/${CONF}
     echo -e "export \"JAVA_OPTS=\$JAVA_OPTS -Dtomcat.port.jvmroute=\$sia_instname\"" >> ${INSTDIR}/${CONF} 
-
+    
+    cat .javaopts >> ${INSTDIR}/${CONF}
     cp ${INSTROOT}/share/server.xml.dist ${INSTDIR}/${CONFDIR}server.xml
 
 } 
@@ -96,7 +128,24 @@ function removeInst() {
     `rm -rf ${INSTDIR}`
 }
 
-
+function _checkArgv()
+{ 
+    echo -e "\e[32mWill you create this instance  : \e[33m${INSTNAME}\e[0m" 
+    read -r -p "Continue[Y/n] : " response
+    case ${response} in
+            [yY][eE][sS]|[yY])
+                echo -e "\e[32mStart create instance \e[33m"${INSTNAME}"...\e[0m" 
+                ;;
+            [nN][oO]|[nN]) 
+                echo -e "\e[93mExit script\e[0m" 
+                exit
+                ;;
+            *)  
+                echo -e "\e[93mExit script\e[0m" 
+                exit
+                ;;
+        esac  
+} 
 INSTNAME=$1 
 ARGV=$2   
 #currentDir=`pwd`
@@ -107,14 +156,19 @@ CONFIG="sia.conf"
 CONF=${CONFDIR}${CONFIG}     
 
 if [ "$#" -ne 2 ]; then  
-    echo "Usage : instadd.sh [Instance] {add | remove}"
-fi
-case $ARGV in 
+    echo "Usage : instadd.sh [Instance] {add | remove}" 
+    exit
+else 
+    _front 
+    _checkArgv 
 
-add)
-    addInst
-    ;;
-remove)
-   removeInst  
-    ;;
-esac
+    case $ARGV in 
+    add) 
+        _setDefault
+        addInst
+        ;;
+    remove)
+        removeInst  
+        ;;
+    esac 
+fi
--- install.sh
+++ install.sh
@@ -2,34 +2,120 @@
 
 
 currentDir=`pwd`;
-#echo ${JAVA_HOME} 
-#echo ${CLASSPATH}  
-
-
-
 temp_java_home=${JAVA_HOME}
-# check JAVA_OPT 
 
-#if [[ ! ${JAVA_HOME} ]]; then 
-echo -e "APACHE TOMCAT INSTALLATION - SIA"
-echo -n "enter JAVA_HOME for tomcat (default : ${temp_java_home} ) : "
-read a
-if [[ $a ]]; then
-    temp_java_home=${a}    
-fi
-#fi
-  
-`cp share/launcher.sh.dist ./launcher.sh` 
-sed -i -e "s:CHANGE_JAVA_HOME:${temp_java_home}:g" launcher.sh 
+echo ${temp_java_home}
+echo ${CLASSPATH}  
+function _front()
+{
+    TITLE="CHLUX TOMCAT MULII INSTANCE PACK INSTALLER"
+    clear
+    eval printf %.0s\# '{1..'${COLUMNS:-$(tput cols)}'}'; echo    
+    echo -e "" 
+    printf "%*s\n" $(((${#TITLE}+$(tput cols))/2)) "$TITLE"
+    echo -e "\t Version : 1.0" 
+    echo -e "\t Author  : Chlux Co,Ltd."
+    echo -e "\t Release : 25. Dec. 2016" 
+    echo -e "\t Package : APACHE TOMCAT 7.0"
+    echo -e "\t Require : Root Permission (Installation)"
+    echo -e ""
+    eval printf %.0s\# '{1..'${COLUMNS:-$(tput cols)}'}'; echo  
 
-if [ ! -d instnace ]; then 
-    mkdir -p instance
-fi
-#echo ${temp_java_home}  
-#echo -e 'export JAVA_HOME='${temp_java_home} >> .setenv 
-
-# else input JAVA_OPT 
+}
 
 
-#check PATH include java
+# check JAVA_OPT  
+function _setjavahome()
+{ 
+    if [[ ! ${temp_java_home} ]]; then 
+        echo -n -e "\e[32mEnter JAVA_HOME for tomcat (default : \\e[2m${temp_java_home} ) : \e[0m"
+        read a 
+        if [[ $a ]]; then
+            temp_java_home=${a}     
+        else  
+            echo -e "\e[91mYou should enter JAVA_HOME\e[0m"   
+            exit;
+            #_setjavahome;
+        fi  
+    else
+        echo -e "\e[32mTomcat will use this JAVA_HOME : \e[2m${temp_java_home}\e[0m" 
+        read -r -p "Continue[Y/n] : " response 
+        case ${response} in
+            [yY][eE][sS]|[yY])  
+                echo -e "\e[32mTomcat installing ...\e[0m" 
+                ;; 
+            [nN][oO]|[nN]) 
+                temp_java_home=""
+                _setjavahome
+                ;;
+            *) 
+                echo -n -e "\e[32mEnter JAVA_HOME for tomcat (default : \\e[2m${temp_java_home} ) : \e[0m"
+                ;;
+        esac  
+    fi
+} 
 
+function _getjavaversion() 
+{ 
+    echo -e -n "\e[32mChecking JAVA Version (java : ${temp_java_home}/bin/java -version ):"
+    javav=`${temp_java_home}/bin/java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}'` 
+    echo -e ${javav}"\e[0m" 
+
+    if [ -z ${javav} ]; then 
+        echo -e "\e[31mWrong JAVA_HOME path, Please check again JAVA_HOME\e[0m"  
+        exit; 
+    fi 
+} 
+
+function _setjavaopt()
+{  
+    echo -e "\e[32msetup JAVA_OPTS for ${javav} \e[0m"
+   jdk6=$(echo $javav | sed -n '/1.6.0/p')
+   jdk7=$(echo $javav | sed -n '/1.7.0/p')
+   jdk8=$(echo $javav | sed -n '/1.8.0/p')
+   
+    if [ ! -z ${jdk6} ]; then    
+        #SET JAVA_OPT 
+        #echo ${jdk6} 
+        OPT="export \"JAVA_OPTS=\$JAVA_OPTS -Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=1024m\""
+    fi  
+
+    if [ ! -z ${jdk7} ]; then  
+        #SET JAVA_OPT
+        #echo ${jdk7} 
+        OPT="export \"JAVA_OPTS=\$JAVA_OPTS -server -Xms128m -Xmx512m\""
+    fi  
+
+    if [ ! -z ${jdk8} ]; then  
+        #SET JAVA_OPT
+        #echo ${jdk8}
+        OPT="export \"JAVA_OPTS=\$JAVA_OPTS -server -Xms128m -Xmx512m\""
+        #OPT="export \"JAVA_OPTS=\$JAVA_OPTS -Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=1024m\""
+    fi  
+    echo -e ${OPT} > .javaopts
+}
+
+function _setlauncher() 
+{
+    echo -e "\e[32mCreate launcher ... \e[0m"
+    cp share/launcher.sh.dist ./launcher.sh
+    sed -i -e "s:CHANGE_JAVA_HOME:${temp_java_home}:g" launcher.sh 
+
+    if [ ! -d instnace ]; then 
+        mkdir -p instance
+    fi
+}
+
+function _footer() 
+{ 
+    echo -e "\e[32m -- Please  run addinst.sh before run launcher \e[0m"  
+    echo -e "\e[96m #[sudo] sh addinst.sh INSTANCE_NAME [add | remove]\e[0m"
+}
+JAVA_OPTS=""
+_front 
+_setjavahome 
+_getjavaversion  
+_setjavaopt  
+_setlauncher 
+_footer
+exit;   
